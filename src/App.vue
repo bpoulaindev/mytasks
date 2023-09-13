f0b764c6254b2eb21a7bc682dfbeffd144b8ae87
Baptiste POULAIN <baptistepoulain@MAC882.local>
2023-09-13 14:12:53 +0200

    (main): fix title margins

--- src/App.vue
+++ src/App.vue
@@ -4,9 +4,9 @@ import TodoList from "./components/TodoList.vue";
 
 <template>
   <div class="flex w-full h-[calc(100dvh)] sm:justify-center items-center bg-cream sm:px-16 flex-col">
-    <div class="flex items-center sm:mt-10 sm:mb-4">
-      <img src="./assets/mytasks.svg" alt="MyTasks" class="h-4 w-4 mr-1" />
-      <h1 class="text-3xl font-bold text-gray-900">MyTasks</h1>
+    <div class="flex items-center my-2 sm:mt-10 sm:mb-4">
+      <img src="./assets/mytasks.svg" alt="MyTasks" class="h-4 w-4 sm:w-6 sm:h-6 mr-1 sm:mr-2" />
+      <h1 class="text-3xl font-semibold sm:text-4xl text-gray-900">MyTasks</h1>
     </div>
     <TodoList />
   </div>
